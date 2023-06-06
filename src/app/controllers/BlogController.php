8f0a2147f1fed88b42fda35ac5de3cf0bba67f8e
Anuj Maurya <anujmaurya@cedcoss.com>
2023-06-06 12:47:39 +0530

    initial commit

---
+++ src/app/controllers/BlogController.php
@@ -0,0 +1,72 @@
+<?php
+
+use Phalcon\Mvc\Controller;
+
+session_start();
+class BlogController extends Controller
+{
+    // show all the blogs in view
+    public function indexAction()
+    {
+        $output = $this->mongo->blogs->find();
+        $data = [];
+        foreach ($output as $value) {
+            $data[] = $value;
+        }
+        $this->view->data = json_encode($data);
+    }
+    public function addAction()
+    {
+        // redirected to view
+    }
+
+    public function displaySingleAction()
+    {
+        $id = $_GET['id'];
+        $output = $this->mongo->blogs->findOne(['id' => (string)$id]);
+        $this->view->data = json_encode($output);
+    }
+
+    public function editAction()
+    {
+        $id = $_GET['id'];
+        $output = $this->mongo->blogs->findOne(['id' => (string)$id]);
+        $this->view->data = json_encode($output);
+    }
+
+    public function updateAction()
+    {
+        $id = $_GET['id'];
+        $output = $this->mongo->blogs->updateOne(
+            ['id' => $id],
+            ['$set' => $_POST]
+        );
+        $success = $output->getModifiedCount();
+        if ($success > 0) {
+            $this->response->redirect('/blog/');
+        } else {
+            echo "<h3>There was some error</h3>";
+            die;
+        }
+    }
+    public function addBlogAction()
+    {
+        if ($_POST['title'] != '' && $_POST['content'] != '') {
+            $_POST['id'] = uniqid();
+            $_POST['author']['id'] = $_SESSION['uid'];
+            $_POST['author']['name'] = $_SESSION['name'];
+            $_POST['date'] = date('Y-m-d');
+            $output = $this->mongo->blogs->insertOne($_POST);
+            $success = $output->getInsertedCount();
+            if ($success > 0) {
+                $this->response->redirect('/blog/');
+            } else {
+                echo "<h3>There was some error</h3>";
+                die;
+            }
+        } else {
+            echo "<h3>Values can't be empty</h3>";
+            die;
+        }
+    }
+}
---
+++ src/app/controllers/IndexController.php
@@ -0,0 +1,51 @@
+<?php
+
+use Phalcon\Mvc\Controller;
+
+session_start();
+class IndexController extends Controller
+{
+    public function indexAction()
+    {
+        // redirected to view
+    }
+
+    public function signupAction()
+    {
+        if ($_POST['name'] != '' && $_POST['email'] != '' && $_POST['password'] != '') {
+            $_POST['uid'] = uniqid();
+            $output = $this->mongo->users->insertOne($_POST);
+            $success = $output->getInsertedCount();
+            if ($success > 0) {
+                $this->response->redirect('/index/login');
+            } else {
+                echo "There was some error";
+                die;
+            }
+        } else {
+            echo "<h3>Please fill all the details !</h3>";
+            die;
+        }
+    }
+
+    public function loginAction()
+    {
+        // redirected to view
+    }
+
+    public function doLoginAction()
+    {
+        $output = $this->mongo->users->findOne(
+            ['email' => $_POST['email'], 'password' => $_POST['password']]
+        );
+        if ($output['name'] != '') {
+            $_SESSION['uid'] = $output['uid'];
+            $_SESSION['role'] = $output['role'];
+            $_SESSION['name'] = $output['name'];
+            $this->response->redirect('/blog/');
+        } else {
+            echo "<h3>Invalid Credentials</h3>";
+            die;
+        }
+    }
+}
